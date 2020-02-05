934145e59278360b4683f120d53f2c9e1c3f2876
resfari <49478541+resfari@users.noreply.github.com>
2020-02-05 19:52:06 +0300

    Add files via upload
    Work well

--- board_threading.py
+++ board_threading.py
@@ -18,10 +18,26 @@ Where is:
 ########################################################################
 """
 
+class Changes():
+    """
+    Класс предназначен для отслеживания состояния доски
+    есть изменения состояния или нет
+    Нужно для выхода из потоков
+    """
+    def __init__(self):
+        self.ch = True
 
-def print_usage():
+def print_usage(num): 
     print(USAGE)
-    exit()
+    if num == 1:
+        print("Wrong numbers in args")
+    elif num == 2:
+        print("Too many arguments")
+    elif num == 3:
+        print("Error in file")
+    elif num == 4:
+        print("M and N ")
+    exit(1)
 
 
 def check_massive(mass):
@@ -39,43 +55,38 @@ def check_size_mass(mass):
     return False
 
 
-def generate_board(m, n, flag_p, path):
-    '''
-    Функция либо генерирует доску(board[M]][N]) и заполняет рандомно 0-ем или 1-ей,
-    либо считывает из файла и помещает в массив board.
-    Также проводится проверка валидности файла
-    '''
+def generate_board(m, n):
     board = []
-    if flag_p == True:
-        try:
-            with open(path, "r") as f:
-                loop = True
-                while loop:
-                    s = list(map(int, f.readline().split()))
-                    if check_massive(s):
-                        raise Exception
-                    if s == []:
-                        loop = False
-                    else:
-                        board.append(s)
-                if check_size_mass(board):
-                    raise Exception
-                return board, len(board), len(board[0])
-        except:
-            print_usage()
-    else:
-        for j in range(m):
+    for j in range(m):
             board.append([random.randint(0, 1) for i in range(n)])
-        return board, m, n
+    return board, m, n
+
+
+def create_from_path(path):
+    board = []
+    try:
+        with open(path, "r") as f:
+            loop = True
+            while loop:
+                s = list(map(int, f.readline().split()))
+                if check_massive(s):
+                    raise Exception
+                if s == []:
+                    loop = False
+                else:
+                    board.append(s)
+            if check_size_mass(board):
+                raise Exception
+            return board, len(board), len(board[0])
+    except:
+        print_usage(3)
 
 
 def summ_connections(board, y, x, m, n):
-    '''
-    y и x настоящее положение клетки
-    m и n размеры доски
-    функция провераяет сколько соседей живых,
-    если он станет равным 4 можно выходить из функции(следует из задания)
-    '''
+    """
+    Функция подсчитывает кол-во живых соседей и выходит
+    при количестве > 3
+    """
     res = 0
     if y > 0 and x > 0:
         res += board[y - 1][x - 1]
@@ -109,14 +120,8 @@ def change_status(summ_conn, status):
             return 0
 
 
-def check_board(board, m, n, status_cell):
-    '''
-    Обновляем статус каждой клетки на борде
-    Если изменений состояния(changes) на доске не произошло,
-    то выводится последнее состояние, программа завершается.
-    '''
+def check_board(board, m, n):
     changes = False
-    status_cell[0], status_cell[1] = 0, 0
     for i in range(m):
         for j in range(n):
             summ_conn = summ_connections(board, i, j, m, n)
@@ -124,124 +129,128 @@ def check_board(board, m, n, status_cell):
             board[i][j] = change_status(summ_conn, board[i][j])
             if prev != board[i][j]:
                 changes = True
-            if board[i][j] == 1:
+    return board, changes
+
+
+def print_status_cell(board):
+    """
+    Функция подсчитывает количество живых клеток
+    """
+    status_cell = [0, 0]
+    for i in board:
+        for j in i:
+            if j == 1:
                 status_cell[0] += 1
             else:
                 status_cell[1] += 1
-    return board, status_cell, changes
+    print("Cell alive = {}\nCell died = {}".format(status_cell[0], status_cell[1]))
 
 
-def print_board(board, cases, status_cell):
-    for i in board:
-        print(*i)
+def print_board(board, cases):
+    mass = []
+    for j, i in enumerate(board):
+        mass.append(i)
+        print(*mass[j])
+    print_status_cell(mass)
     if cases == 1:
-        print("\n")
-    elif cases == 2:
-        print("Cell alive = {}\nCell died = {}".format(status_cell[0], status_cell[1]))
-    elif cases == 3:
         print("No more changes on board")
+    if cases == 2:
+        print("Programm ended")
 
 
+def thread_1(board, m, n, changes):
+    """
+    Поток-1 постоянно изменяет состояние доски
+    """
+    while changes.ch:
+            board, changes.ch = check_board(board, m, n)
+    
 
-def thread_1(board, m, n, changes, status_cell):
-    while changes:
-        try:
-            board, status_cell, changes = check_board(board, m, n, status_cell)
-        except KeyboardInterrupt:
-            print_board(board, 3, None)
-            exit(1)
-    exit(1)
-
-def thread_2(check_time, board, changes, status_cell):
-    while changes:
-        try:
+def thread_2(board, changes):
+    """
+    Поток-2 спит 1 секунду, затем выводит доску и кол-во живых клеток
+    """
+    while changes.ch:
             time.sleep(1)
-            print_board(board, 2, status_cell)
-        except KeyboardInterrupt:
-            exit(1)
-    exit(1)
-
+            print_board(board, 0)
+    print_board(board, 1)
 
 
 def programm(board_size, flag_p, path):
-    '''
-    Функция отвечает за состояние доски и вывод ее в консоль каждую секунду.
-    В случае, когда изменений нет, завершается программа.
-    '''
-    m = board_size[0]
-    n = board_size[1]
+    """
+    Основная функция
+    Распределение потоков:
+        thread1 - изменяет состояние доски(board[])
+        thread2 - выводит состояние доски каждую секунду(board[])
+    Выход из потоков осуществляет при отсутствии изменений состояний доски
+    в ином случае посредством CTRL+C
+    """
+    if not flag_p:
+        board, m, n = generate_board(board_size[0], board_size[1])
+    else:
+        board, m, n = create_from_path(path)
     status_cell = [0, 0]
-    board, m, n = generate_board(m, n, flag_p, path)
-    print_board(board, 1, None)
-    check_time = time.time()
-    mass = [True]
-    thread1 = Thread(target=thread_2, args=(check_time, board, mass[0], status_cell), daemon=True)
-    thread2 = Thread(target=thread_1, args=(board, m, n, mass[0], status_cell), daemon=True)
+    print_board(board, 0)
+    changes = Changes()
+    thread1 = Thread(target=thread_2, args=(board, changes), daemon=True)
+    thread2 = Thread(target=thread_1, args=(board, m, n, changes), daemon=True)
     try:
-        thread2.start()
         thread1.start()
-        while True:
-            time.sleep(100)
+        thread2.start()
+        thread1.join()
+        thread2.join()
     except KeyboardInterrupt:
-        print("The end")
-
-    #thread1.join()
-    #thread2.join()
-    exit(1)
+        print_board(board, 2)
 
 
 def check_arg(arg):
-    '''
-    Проверка, что в аргументе только число
-    Пример:
-    python3 board.py 10 20s  - вызовет Usage
-    '''
     try:
+        if int(arg) < 1:
+            raise Exception
         for i in arg:
             if not i.isdigit():
                 raise Exception
-            if int(arg) <= 1:
-                raise Exception
     except:
-        print_usage()
+        print_usage(1)
     return True
 
 
-def main():
-    '''
-    Проверка аргументов sys.args
-    От этого зависит нужно генерировать доску или считывать из файла
-    Так же если даны M и N и включен флаг -p, вызывается ошибка(Usage в консоль)
-    Пример:
-    python3 board.py 10 20 ./Desctop/example.txt
-    '''
+def validating():
+    """
+    Проверка входящих аргументов(кол-во, конфликты флагов)
+    """
     board_size = [0, 0]
     count = 0
-    flag_p = False
-    flag_no_path = False
-    path = ""
+    flag_p, flag_no_path, count, path = False, False, 0, ""
+    if len(sys.argv) < 3:
+        print("No arguments")
+        exit(1)
     for i in range(1, len(sys.argv)):
         if count > 1:
-            print_usage()
+            print_usage(2)
         elif i == "-help":
-            print_usage()
+            print_usage(0)
         elif sys.argv[i] == "-p":
             if flag_no_path == True:
-                print_usage()
+                print_usage(4)
             elif i + 1 == len(sys.argv) - 1:
                 flag_p = True
                 path = sys.argv[i + 1]
                 break
             else:
-                print_usage()
+                print_usage(0)
         elif check_arg(sys.argv[i]):
             if flag_p == True:
-                print_usage()
+                print_usage(4)
             flag_no_path = True
             board_size[count] = int(sys.argv[i])
             count += 1
         else:
-            print_usage()
+            print_usage(0)
+    return board_size, flag_p, path
+
+def main():
+    board_size, flag_p, path = validating()
     programm(board_size, flag_p, path)
 
 
